1a65d196ff4756f8c8b56796bb23a397990a7816
briandriscollcode <bdriscoll407@gmail.com>
2023-02-21 17:20:10 -0700

    test: added aws connection to serverless function for netlify v2

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,12 +2,20 @@
 exports.handler = async function (event, context) {  // eslint-disable-line
     const postgres = require("postgres");
 
+    // const db = postgres({
+    //     host: process.env.PGHOST,
+    //     port: process.env.PGPORT,
+    //     user: process.env.PGUSER,
+    //     password: process.env.PGPASSWORD,
+    //     database: process.env.PGDATABASE
+    // });
+
     const db = postgres({
-        host: process.env.PGHOST,
-        port: process.env.PGPORT,
-        user: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE
+        host: "vue-reference-test.clphvgvftlzr.us-west-1.rds.amazonaws.com",
+        port: 5432,
+        user: "master_dev",
+        password: "Imcool123!",
+        database: "vue_reference_test1"
     });
 
     const data = await db`SELECT * FROM account`;
