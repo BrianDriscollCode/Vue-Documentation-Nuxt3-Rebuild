2044b64e0d76c92567ac808563ce81aecd1d44aa
briandriscollcode <bdriscoll407@gmail.com>
2023-02-21 18:38:25 -0700

    test: testing another env setup with netlify env v4

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -6,11 +6,11 @@ exports.handler = async function (event, context) {  // eslint-disable-line
     // const config = useRuntimeConfig();
 
     const db = postgres({
-        host: AWSHOST,
-        port: AWSPORT,
-        user: AWSUSER,
-        password: AWSPASSWORD,
-        database: AWSDATABASE
+        host: process.env.AWSHOST,
+        port: process.env.AWSPORT,
+        user: process.env.AWSUSER,
+        password: process.env.AWSPASSWORD,
+        database: process.env.AWSDATABASE
     });
 
     const data = await db`SELECT * FROM account`;
